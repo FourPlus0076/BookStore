a14743ec785ade91fc54785f65e1270e4047e672
gausm <gausm@LAPTOP-91RABLC6>
2023-07-08 22:48:45 +0530

    Added Book Controller

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Models;
+using BookStore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<BookModel> GetAllBook()
+        { 
+          return _bookRepository.GetAllBook();
+        }
+        public BookModel GetBook(int Id) 
+        {
+          return _bookRepository.GetBook(Id);
+        }
+    }
+}
---
+++ BookStore/Repositories/BookRepository.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Repositories
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBook()
+        {
+            return DataSource();
+        }
+        public BookModel GetBook(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=1,Title="MVC", Name="gm"},
+                new BookModel(){ Id=2,Title="C#", Name="gm1"},
+                new BookModel(){ Id=3, Title="Java", Name="jm"},
+                new BookModel(){ Id=4,Title="html",Name="name"},
+                new BookModel(){ Id=5, Title="php", Name="gaus"}
+            };
+        }
+    }
+}
