2dad94bbc1ba0236f833c7ae6ab194b0835528a2
gausm <gausm@LAPTOP-91RABLC6>
2023-07-12 11:30:40 +0530

    Added viewData asp.net core

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -7,7 +7,12 @@ namespace BookStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]       
+        public string Title { get; set; }
+        [ViewData]
+        public BookModel bookModel { get; set; }
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -15,6 +20,15 @@ namespace BookStore.Controllers
 
         public IActionResult Index()
         {
+            CustomProperty = "Gaus This Side";
+            Title = "Home Page from controller";
+            //var list = "Gaus This Side";
+            //ViewBag.List = list;    
+            bookModel = new BookModel()
+            {
+                Id = 1,
+                Name = "BCA",
+            };
             return View();
             //return View("TempView/GausTemp.cshtml");
         }
