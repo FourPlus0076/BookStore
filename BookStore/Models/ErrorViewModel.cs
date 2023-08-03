ffbc325e37411d51a9f357bc366e513506601d46
gausm <gausm@LAPTOP-91RABLC6>
2023-08-03 12:26:39 +0530

    AdddedAreaswithAdmin

--- BookStore/Models/ErrorViewModel.cs
+++ BookStore/Models/ErrorViewModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Models
 {
     public class ErrorViewModel
     {
+        [Required]
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
